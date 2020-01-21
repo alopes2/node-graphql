108a8db9ed69921dff1ec667322c34586e2e59b7
Andre Lopes <andrelo@dunnhumby.com>
2020-01-21 10:28:30 +0100

    Add signup and login support

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,82 @@
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+
+const keys = require('../config/keys');
+const User = require('../models/user');
+
 module.exports = {
-  hello() {
+  // createUser(args, req) {
+  createUser: async function({ userInput }, req) {
+    const { email, name, password } = userInput;
+    const errors = [];
+    console.log(password);
+
+    if (!validator.isEmail(email)) {
+      errors.push({ message: 'Email is invalid.' });
+    }
+
+    if (
+      validator.isEmpty(password) ||
+      !validator.isLength(password, { min: 5 })
+    ) {
+      errors.push({ message: 'Pasword too short.' });
+    }
+
+    if (errors.length > 0) {
+      const error = new Error('Invalid input.');
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      const error = new Error('User exists already!');
+      throw error;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = new User({
+      email: email,
+      name: name,
+      password: hashedPassword
+    });
+
+    const createdUser = await user.save();
+
     return {
-      text: 'Hello World',
-      views: 1234
+      ...createdUser._doc,
+      _id: createdUser._id.toString()
     };
+  },
+  login: async function({ email, password }) {
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      const error = new Error('User not found.');
+      error.code = 401;
+      throw error;
+    }
+
+    const passwordIsCorrect = await bcrypt.compare(password, user.password);
+
+    if (!passwordIsCorrect) {
+      const error = new Error('Password is incorrect.');
+      error.code = 401;
+      throw error;
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        email: user.email
+      },
+      keys.jwtSecret,
+      { expiresIn: '1h' }
+    );
+
+    return { token: token, userId: user._id.toString() };
   }
-}
+};
