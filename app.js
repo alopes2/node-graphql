b72fd940cb9bee18d502162a98eda73a2a6f054c
Andre Lopes <andrelo@dunnhumby.com>
2020-01-21 10:28:08 +0100

    Add start configuration

--- app.js
+++ app.js
@@ -22,13 +22,32 @@ app.use((req, res, next) => {
   );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
   next();
 });
 
-app.use('/graphql', graphqlHttp({
-  schema: graphqlSchema,
-  rootValue: graphqlResolver
-}));
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+
+      const data = err.originalError.data;
+      const message = err.message || 'An error occurred';
+      const code = err.originalError.code || 500;
+
+      return { message: message, status: code, data: data };
+    }
+  })
+);
 
 app.use((error, req, res, next) => {
   console.log(error);
